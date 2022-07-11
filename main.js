e7a0f08de6a4f3e5172b117159a1a31bfe5f57bd
German Silva <german.silva.96@hotmail.com>
2022-07-10 21:04:48 -0500

    modify for sumagolestotalequipo, variable level not are the correct

--- main.js
+++ main.js
@@ -5,27 +5,33 @@ async function obtenerSueldos() {
   //For para sumar los goles de cada jugador y asignarlos a la variable sumaDeGolesDelEquipo
   for (let i = 0; i <= json.jugadores.length; i++) {
     let sumaDeGolesDelEquipo = 0;
-
+    
     for (let j = 0; j < json.jugadores.length; j++) {
       sumaDeGolesDelEquipo += json.jugadores[j]?.goles;
     } //console.log(sumaDeGolesDelEquipo);
 
+    
+    
     //For para asignar los goles minimos de cada jugador y sumar el total de goles que debe tener el equipo
     let sumaGolesTotalesEquipo = 0;
-    for (let k = 0; k < json.jugadores.length; k++) {
-      if (json.jugadores[k]?.nivel === "A") {
+    for(let k = 0; k < json.jugadores.length; k++){
+        if (json.jugadores[k]?.nivel === "A"){
         json.jugadores[k].goles_minimos = 5;
-      } else if (json.jugadores[k]?.nivel === "B") {
+    }
+        else if (json.jugadores[k]?.nivel === "B"){
         json.jugadores[k].goles_minimos = 10;
-      } else if (json.jugadores[k]?.nivel === "C") {
+    }
+        else if (json.jugadores[k]?.nivel === "C"){
         json.jugadores[k].goles_minimos = 15;
-      } else if (json.jugadores[k]?.nivel === "El Cuauh") {
+    }
+        else if (json.jugadores[k]?.nivel === "Cuauh"){
         json.jugadores[k].goles_minimos = 20;
-      }
-
-      sumaGolesTotalesEquipo += json.jugadores[k].goles_minimos;
     }
 
+        sumaGolesTotalesEquipo += json.jugadores[k].goles_minimos;
+    }
+    
+    
     const nivel = json.jugadores[i]?.nivel;
     const goles = json.jugadores[i]?.goles;
     const sueldo = json.jugadores[i]?.sueldo;
@@ -33,28 +39,42 @@ async function obtenerSueldos() {
     const golesMinimos = json.jugadores[i]?.goles_minimos;
 
     //Alcance de Jugador
-    const alcanceJugador = goles / golesMinimos;
+    const alcanceJugador = (goles / golesMinimos);
 
     //Alcance del equipo
-    const alcanceDelEquipo = sumaDeGolesDelEquipo / sumaGolesTotalesEquipo;
+    const alcanceDelEquipo = (sumaDeGolesDelEquipo / sumaGolesTotalesEquipo);
 
     //Alcance total del jugador
     const alcanceTotaldeJugador = (alcanceJugador + alcanceDelEquipo) / 2;
 
     //calculando sueldo completo
+    const sueldoCompleto = alcanceTotaldeJugador * bono
+    //Cabe recalcar que la llave sueldo_completo no es el sueldo completo del jugador, si no solo el calculo del bono que le toca, Agrego la llave sueldo_con_bono para calcular el sueldo completo
 
-    const sueldoCompleto = alcanceTotaldeJugador * bono;
+    const sueldoBono = sueldoCompleto + sueldo;
 
     if (json.jugadores[i]?.nivel === "A") {
-      json.jugadores[i].sueldo_completo = sueldoCompleto;
+      console.log(sueldoCompleto);
+      json.jugadores[i].sueldo_completo = sueldoCompleto
+      console.log(sueldoBono);
+      json.jugadores[i].sueldo_con_bono = sueldoBono
       console.log(json.jugadores);
     } else if (json.jugadores[i]?.nivel === "B") {
-      json.jugadores[i].sueldo_completo = sueldoCompleto;
-    } else if (json.jugadores[i]?.nivel === "C") {
-      json.jugadores[i].sueldo_completo = sueldoCompleto;
+        console.log(sueldoCompleto);
+      json.jugadores[i].sueldo_completo = sueldoCompleto
+      console.log(sueldoBono);
+      json.jugadores[i].sueldo_con_bono = sueldoBono
+    } else if (json.jugadores[i]?.nivel === "C") { 
+        console.log(sueldoCompleto);
+        json.jugadores[i].sueldo_completo = sueldoCompleto
+        console.log(sueldoBono);
+      json.jugadores[i].sueldo_con_bono = sueldoBono
     } else if (json.jugadores[i]?.nivel === "Cuauh") {
-      json.jugadores[i].sueldo_completo = sueldoCompleto;
-    }
+        console.log( parseInt(sueldoCompleto));
+        json.jugadores[i].sueldo_completo = sueldoCompleto
+        console.log(sueldoBono);
+      json.jugadores[i].sueldo_con_bono = sueldoBono
+    } 
   }
 }
 
